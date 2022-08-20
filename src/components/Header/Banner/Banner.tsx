ac1423b85f7e33f804268ebce2b7bbed706b7823
TheLocagus <locagus@o2.pl>
2022-08-20 13:49:35 +0200

    Added background image on section banner

--- src/components/Header/Banner/Banner.tsx
+++ src/components/Header/Banner/Banner.tsx
@@ -4,19 +4,17 @@ import './Banners.scss';
 
 export const Banner = () => {
 
-    return (
-        <>
-            <div className="banner">
-                <div className="title">
-                    <h1>O mnie</h1>
-                </div>
-                <div className="content">
-                    <p className='first-part'>Witam, nazywam się Bartosz Kołsut i chcę zostać Junior Javascript Developerem.</p>
-                    <p className='second-part'>Posiadam fullstackowe zaplecze technologiczne dzięki czemu uważam, że odnajdę się zarówno na froncie jak i na backendzie. </p>
-                    <p className='third-part'>Oczywiście nie jestem ekspertem, jeszcze wiele nauki przede mną, a nigdzie się tak nie rozwinę jak w środowisku doświadczonych programistów!</p>
-                </div>
-            </div>
-        </>
-
-    )
+  return (
+    <>
+      <div className="banner" style={{backgroundImage: 'url(programmer-background.png)'}}>
+        <div className="content" >
+          <p className='first-part'>Witam, nazywam się Bartosz Kołsut i chcę zostać Junior Javascript Developerem.</p>
+          <p className='second-part'>Posiadam fullstackowe zaplecze technologiczne dzięki czemu uważam, że odnajdę się
+            zarówno na froncie jak i na backendzie. </p>
+          <p className='third-part'>Oczywiście nie jestem ekspertem, jeszcze wiele nauki przede mną, a nigdzie się tak
+            nie rozwinę jak w środowisku doświadczonych programistów!</p>
+        </div>
+      </div>
+    </>
+  )
 }
