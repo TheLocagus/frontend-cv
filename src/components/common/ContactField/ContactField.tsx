e30ce550f873733f453aeb7c33c9d762c642bc16
TheLocagus <locagus@o2.pl>
2022-08-22 22:28:55 +0200

    Implemented toastify.

--- src/components/common/ContactField/ContactField.tsx
+++ src/components/common/ContactField/ContactField.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { DynamicButtonEnum } from 'types';
 
 import './ContactField.scss';
+import {toast, ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 interface Props {
   imgName: string;
   type: DynamicButtonEnum;
@@ -12,9 +14,19 @@ interface Props {
 
 export const ContactField = ({imgName, type, value, url}: Props) => {
 
+  const notify = () => toast.info('Skopiowano!', {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+
   const copyText = (value: string) => {
     navigator.clipboard.writeText(value)
-    alert("Skopiowano")
+    notify();
   }
 
   return (
@@ -26,7 +38,19 @@ export const ContactField = ({imgName, type, value, url}: Props) => {
         {
           type === 'a'
             ? <a href={url}>{value}</a>
-            : <button onClick={() => copyText(value)}>{value}</button>
+            : (<>
+              <button onClick={() => copyText(value)}>{value}</button>
+              <ToastContainer
+                position="top-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+              /></>)
         }
       </div>
     </div>
